df792dae0333a53807a7c9751604c7a6bd047af3
WHenderson <whenderson.github@gmail.com>
2017-01-08 22:51:40 +1100

    Fixing folder structure, registering on pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hashdb2',
+      version='0.1',
+      description='HashDb2 provides a simple method for executing commands based on matched files',
+      url='https://github.com/WHenderson/HashDb',
+      author='Will Henderson',
+      author_email='whenderson.github@gmail.com',
+      license='Apache 2.0',
+      packages=['hashdb2'],
+      zip_safe=False)
