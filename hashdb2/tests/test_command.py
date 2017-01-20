3921f60c2700b0c51004548d6cb986f0a880f61e
WHenderson <whenderson.github@gmail.com>
2017-01-21 09:13:30 +1100

    Validate output

--- hashdb2/tests/test_command.py
+++ hashdb2/tests/test_command.py
@@ -2,6 +2,8 @@ from unittest import TestCase
 
 from hashdb2.command_line import main
 from docopt import DocoptExit
+from contextlib import redirect_stdout, redirect_stderr
+from io import StringIO
 
 class TestCommand(TestCase):
     def test_help(self):
@@ -11,4 +13,8 @@ class TestCommand(TestCase):
 
     def test_version(self):
         with self.assertRaises(SystemExit) as context:
-            main(['--version'])
+            out = StringIO()
+            with redirect_stdout(out):
+                main(['--version'])
+
+        self.assertTrue('HashDb' in out.getvalue())
