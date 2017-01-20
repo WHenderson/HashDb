3e4c485314f5b04c7c1437e8db48331e7e183b61
WHenderson <whenderson.github@gmail.com>
2017-01-21 08:14:04 +1100

    Adding first dependency

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
-setup(name='hashdb2',
-      version='0.2',
-      description='HashDb2 provides a simple method for executing commands based on matched files',
-      url='https://github.com/WHenderson/HashDb',
-      author='Will Henderson',
-      author_email='whenderson.github@gmail.com',
-      license='Apache 2.0',
-      packages=['hashdb2'],
-      zip_safe=False)
+setup(
+    name='hashdb2',
+    version='0.2',
+    description='HashDb2 provides a simple method for executing commands based on matched files',
+    url='https://github.com/WHenderson/HashDb',
+    author='Will Henderson',
+    author_email='whenderson.github@gmail.com',
+    license='Apache 2.0',
+    packages=['hashdb2'],
+    zip_safe=False,
+    install_requires=[
+        'docopt>=0.6.2'
+    ]
+)
