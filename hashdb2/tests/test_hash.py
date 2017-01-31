b424f22ffda8c46a251b7960e9cf2f22218d950b
WHenderson <whenderson.github@gmail.com>
2017-01-31 17:30:47 +1100

    Validating hash

--- hashdb2/tests/test_hash.py
+++ hashdb2/tests/test_hash.py
@@ -7,20 +7,30 @@ from .generate_files import generate_files, generate_structure, structures
 import os.path
 from sqlalchemy import MetaData, Table, func, and_
 
-
 class TestHash(TestCase):
-    def test_none(self):
-        with generate_structure(structures['complex']) as root:
-            dbPath = os.path.join(root, 'hash.db')
-            inputRoot = os.path.join(root, 'inputs')
+    def test_structures(self):
+        for name, structure in structures.items():
+            with generate_structure(structure) as root:
+                dbPath = os.path.join(root, 'hash.db')
+                inputRoot = os.path.join(root, 'inputs')
 
-            main(['hash', '-n', dbPath, '--', inputRoot])
+                main(['hash', '-n', dbPath, '--', inputRoot])
 
-            engine = create(dbPath)
-            metadata = MetaData()
-            Files = Table('Files', metadata, autoload=True, autoload_with=engine)
+                engine = create(dbPath)
+                metadata = MetaData()
+                Files = Table('Files', metadata, autoload=True, autoload_with=engine)
 
-            for file in engine.execute(Files.select()):
-                print(file)
+                inputFiles = [
+                    (os.path.normpath(file.path), os.path.basename(file.path), os.path.splitext(file.path)[1], len(file.data), file.time, None, None)
+                    for file in structure if isinstance(file.data, bytes) or isinstance(file.data, bytearray)
+                ]
+                inputFiles.sort()
+                outputFiles = [
+                    (os.path.normpath(os.path.relpath(file.path, root)), os.path.basename(file.path), os.path.splitext(file.path)[1], file.size, file.time, None, None)
+                    for file in engine.execute(Files.select())
+                ]
+                outputFiles.sort()
 
+
+                self.assertEqual(inputFiles, outputFiles)
 
