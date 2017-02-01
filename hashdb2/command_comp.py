16b4ad281b06d0b913b92a7f483524f1680fc7dc
WHenderson <whenderson.github@gmail.com>
2017-02-01 15:56:18 +1100

    Adding input combinations

--- hashdb2/command_comp.py
+++ hashdb2/command_comp.py
@@ -20,9 +20,6 @@ def is_subpath(column, subpath):
         return func.SUBSTR(column, 1, len(subpathdir)) == subpathdir
 
 def attach_side(engine, side, dbpath, update, subpath):
-    # ToDo: Work out creating Views and sort out schema names (attached db is in one schema, the other tables are in the main schema)
-    # I *should* be able to have cross-database views
-
     db = side + 'db'
     rw = side + 'rw'
     ro = side + 'ro'
@@ -50,28 +47,54 @@ def attach_side(engine, side, dbpath, update, subpath):
                 # ro = view of lhsrw
                 pass
 
-            attach(engine, ro)
             engine.execute(CreateView(ro, sel))
+            roFiles = Table(ro, MetaData(), autoload=True, autoload_with=engine)
 
-            pass
-        elif subpath:
-            # db = actual database
-            # rw = LHSPATH restricted copy of lhsdb
-            # ro = view of lhsrw
-            pass
+            return rwFiles, roFiles
         else:
-            # db = actual database
-            # rw = copy of lhsdb
-            # ro = view of lhsrw
-            pass
+            attach(engine, db, dbpath)
+            dbFiles = Table('Files', MetaData(schema=db), autoload=True, autoload_with=engine)
+
+            attach(engine, rw)
+            create_schema(engine, rw)
+            rwFiles = Table('Files', MetaData(schema=rw), autoload=True, autoload_with=engine)
+
+            engine.execute(CreateView(ro, rwFiles.select()))
+            roFiles = Table(ro, MetaData(), autoload=True, autoload_with=engine)
+
+            sel = dbFiles.select()
+
+            if subpath:
+                # db = actual database
+                # rw = LHSPATH restricted copy of lhsdb
+                # ro = view of lhsrw
+                sel = sel.where(is_subpath(dbFiles.c.path, subpath))
+            else:
+                # db = actual database
+                # rw = copy of lhsdb
+                # ro = view of lhsrw
+                pass
+
+            rwFiles.insert().from_select(dbFiles.c.values(), sel)
+
+            return rwFiles, roFiles
     else:
         # rw = fresh memory database
         # ro = view f lhsrw
-        pass
+
+        attach(engine, rw)
+        create_schema(engine, rw)
+        rwFiles = Table('Files', MetaData(schema=rw), autoload=True, autoload_with=engine)
+
+        engine.execute(CreateView(ro, rwFiles.select()))
+        roFiles = Table(ro, MetaData(), autoload=True, autoload_with=engine)
+
+        return rwFiles, roFiles
+
 
 def command_comp(arguments):
     engine = create(None)
     with engine_dispose(engine):
-        attach_side(engine, 'lhs', arguments['--lhs-db'], arguments['--lhs-update'], arguments['--lhs-path'])
-        attach_side(engine, 'rhs', arguments['--rhs-db'], arguments['--rhs-update'], arguments['--rhs-path'])
+        lhsrwFiles, lhsroFiles = attach_side(engine, 'lhs', arguments['--lhs-db'], arguments['--lhs-update'], arguments['--lhs-path'])
+        rhsrwFiles, rhsroFiles = attach_side(engine, 'rhs', arguments['--rhs-db'], arguments['--rhs-update'], arguments['--rhs-path'])
 
